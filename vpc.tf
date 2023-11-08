0dfd2310185091e6f9b1cda8beae00384366544d
Saideva12 <saidevabhakthuni@gmail.com>
2023-11-08 10:52:48 +0000

    Added Pub Subnet A&B

--- vpc.tf
+++ vpc.tf
@@ -6,3 +6,23 @@ resource "aws_vpc" "tf-ecomm-vpc" {
     Name = "ecomm-vpc"
   }
 }
+
+resource "aws_subnet" "tf-ecomm-pub-sub-A" {
+  vpc_id     = aws_vpc.tf-ecomm-vpc.id
+  cidr_block = "192.62.0.0/24"
+  availability_zone = "us-east-2a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "ecomm-public-sub-A"  
+  }
+}
+
+resource "aws_subnet" "tf-ecomm-pub-sub-B" {
+  vpc_id     = aws_vpc.tf-ecomm-vpc.id
+  cidr_block = "192.62.1.0/24"
+  availability_zone = "us-east-2b"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "ecomm-public-sub-B"
+  }
+}
